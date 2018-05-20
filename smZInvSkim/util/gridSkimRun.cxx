68670a1ff0dcfdb9f67b98c7a7a4be9909cba1fc
Hyungsuk Son <hyungsuk.son@cern.ch>
2018-05-19 21:41:06 -0400

    Add athena packages EventLoop and EventLoopGrid

--- smZInvSkim/util/gridSkimRun.cxx
+++ smZInvSkim/util/gridSkimRun.cxx
@@ -99,16 +99,16 @@ int main( int argc, char* argv[] ) {
 //  EL::GridDriver driver; //grid in the background
 
 //  driver.options()->setString("nc_outputSampleName", "user.hson.mc15c.13TeV.Sherpa.EXOT5.ttbar.02122018.%in:name[2]%.%in:name[6]%"); //For PrunDriver
-  driver.options()->setString("nc_outputSampleName", "user.hson.mc16a.13TeV.deriv_skim_test.05112018.%in:name[2]%.%in:name[6]%"); //For PrunDriver
+  driver.options()->setString("nc_outputSampleName", "user.hson.mc16a.13TeV.deriv_skim_test.05192018.%in:name[2]%.%in:name[6]%"); //For PrunDriver
 //  driver.outputSampleName = "user.hson.gridtest1.11142015.%in:name[2]%.%in:name[6]%"; //For GridDriver
-  driver.options()->setDouble("nc_nFiles", 2); // FOR TESTING!
+  driver.options()->setDouble("nc_nFiles", 10); // FOR TESTING!
 //  driver.options()->setDouble("nc_nFilesPerJob", 1);
 //  driver.options()->setDouble(EL::Job::optGridNFilesPerJob, 1);
 //  driver.options()->setDouble(EL::Job::optGridMaxNFilesPerJob, 3);
   driver.options()->setDouble(EL::Job::optGridNGBPerJob, 6);
 //  driver.options()->setDouble(EL::Job::optGridMergeOutput, 0);
-//  driver.options()->setDouble(EL::Job::optGridNGBPerMergeJob, 3);
-  driver.options()->setDouble("nc_nGBPerMergeJob", 3);
+  driver.options()->setDouble(EL::Job::optGridNGBPerMergeJob, 5);
+//  driver.options()->setDouble("nc_nGBPerMergeJob", 3);
 //  driver.options()->setString("nc_excludedSite", "ANALY_SCINET,ANALY_VICTORIA,ANALY_CERN_CLOUD,ANALY_IN2P3-CC,ANALY_LAPP,ANALY_CONNECT_SHORT,ANALY_SFU,ANALY_CONNECT,ANALY_RAL_SL6,ANALY_GRIF-LPNHE,ANALY_HU_ATLAS_Tier2,ANALY_OU_OCHEP_SWT2,ANALY_IFIC,ANALY_ECDF_SL6");
 //  driver.options()->setString("nc_excludedSite", "ANALY_INFN-NAPOLI-RECAS,ANALY_INFN-NAPOLI,ANALY_DESY-HH,ANALY_GRIF-IRFU,ANALY_AUSTRALIA,ANALY_SFU,ANALY_SCINET,ANALY_CPPM,ANALY_SiGNET,ANALY_LPC,ANALY_NSC,ANALY_CONNECT,ANALY_MWT2_SL6,ANALY_BU_ATLAS_Tier2_SL6,ANALY_wuppertalprod,ANALY_ARNES,ANALY_SLAC_SHORT_1HR,ANALY_SLAC,ANALY_RAL_SL6,ANALY_INFN-MILANO-ATLASC");
 //  driver.options()->setString("nc_excludedSite", "ANALY_GOEGRID");
