c97d361a8e6eb7ab65540044a819776446e7c2e7
hson <hyungsuk.son@cern.sh>
2018-10-27 19:20:23 -0400

    update

--- smZInvAnalysis/util/submitBatchAnalRun.cxx
+++ smZInvAnalysis/util/submitBatchAnalRun.cxx
@@ -444,10 +444,10 @@ int main( int argc, char* argv[] ) {
   // For Systematic
   //std::string slurmOptions = "-n 1 --cpus-per-task 1 --mem-per-cpu=50000 -p batch --time=2-2:00:00 --begin=now+2400 --exclude=m4lmem01,alpha018,pcomp18,pmem01,alpha012,pcomp30,pcomp26,omega021,alpha029,alpha028,pcomp01,pmem02 -o stdout.%j -e stderr.%j --mail-type=END --job-name="+slurmJobName;
   // For Non-systematic
-  //std::string slurmOptions = "-n 1 --cpus-per-task 1 --mem-per-cpu=32000 -p batch --time=2-2:00:00 --begin=now+2400 --exclude=m4lmem01,alpha018,pcomp18,pmem01,alpha012,pcomp30,pcomp26,omega021,alpha029,alpha028,pcomp01,pmem02 -o stdout.%j -e stderr.%j --mail-type=END --job-name="+slurmJobName;
+  std::string slurmOptions = "-n 1 --cpus-per-task 1 --mem-per-cpu=32000 -p batch --time=2-2:00:00 --begin=now+2400 --exclude=m4lmem01,alpha018,pcomp18,pmem01,alpha012,pcomp30,pcomp26,omega021,alpha029,alpha028,pcomp01,pmem02 -o stdout.%j -e stderr.%j --mail-type=END --job-name="+slurmJobName;
 
   // For no-systematics using ALTAS QOS (--account=atlas --qos=atlas)
-  std::string slurmOptions = "-n 1 --cpus-per-task 1 --mem-per-cpu=32000 -p batch --time=2-2:00:00 --account=atlas --qos=atlas --begin=now+2400 --exclude=m4lmem01,alpha018,pcomp18,pmem01,alpha012,pcomp30,pcomp26,omega021,alpha029,alpha028,pcomp01,pmem02 -o stdout.%j -e stderr.%j --mail-type=END --job-name="+slurmJobName;
+  //std::string slurmOptions = "-n 1 --cpus-per-task 1 --mem-per-cpu=32000 -p batch --time=2-2:00:00 --account=atlas --qos=atlas --begin=now+2400 --exclude=m4lmem01,alpha018,pcomp18,pmem01,alpha012,pcomp30,pcomp26,omega021,alpha029,alpha028,pcomp01,pmem02 -o stdout.%j -e stderr.%j --mail-type=END --job-name="+slurmJobName;
 
   // Submit batch runs with 1hour delay
   //std::string slurmOptions = "-n 1 --cpus-per-task 1 --mem 32000 -p batch --time=2-2:00:00 --begin=now+1hour --exclude=m4lmem01,alpha018,pcomp18,pmem01,alpha012,pcomp30,pcomp26,omega021 -o stdout.%j -e stderr.%j --mail-type=END --job-name="+slurmJobName;
